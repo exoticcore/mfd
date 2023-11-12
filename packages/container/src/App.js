034b07edefc7f07d70a0fd69cf668f748244cc91
exoticcore <greannakub@gmail.com>
2023-11-12 16:12:06 +0700

    some update

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
+// container micro frontends
 export default () => {
     return <div><h1>Hi there!</h1>
         <hr />

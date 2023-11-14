339a98e43204f1659d1128c612d8406b6d94e85e
exoticcore <greannakub@gmail.com>
2023-11-14 14:29:08 +0700

    radomize production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,21 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
-// container micro frontends
+const generateClassname = createGenerateClassName({
+    productionPrefix: 'co'
+});
+
 export default () => {
     return (
         <BrowserRouter>
-            <div>
-                <Header />
-                <MarketingApp />
-            </div>
+            <StylesProvider generateClassName={generateClassname}>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     );
 };
